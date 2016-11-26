45dc99a4353eb9619908eeb760fb8521a57c4870
Steve Glover <steven.glover@gmail.com>
2016-11-26 18:03:35 +0000

    Remove dist (shouldn't have been checked in)

--- dist/api/services/protocols/basic.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var localProtocol = require('./local');
-
-module.exports = function (req, username, password, next) {
-  sails.log('using basic auth strategy for user', username, ', password', password);
-
-  return localProtocol.login(req, username, password, next);
-};
--- dist/config/policies.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports.policies = {
-  AuthController: {
-    '*': ['passport']
-  }
-};
